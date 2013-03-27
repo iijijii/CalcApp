9e1d2ca692b20f526eb5399a2f9e7c979dae10ec
iijijii <lilysatoo@gmail.com>
2013-03-27 23:12:16 +0900

    エラーページを追加

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import java.util.Set;
+
 import play.*;
 import play.mvc.*;
 import play.data.*;
@@ -11,13 +13,25 @@ public class Application extends Controller {
 	static Form<Calculate> calcForm = form(Calculate.class);
 
 	public static Result index() {
+		// TODO
+		// 全ての計算結果を削除する
 		return ok(views.html.index.render(calcForm));
 	}
 
 	public static Result calculate() {
 		Form<Calculate> filledForm = calcForm.bindFromRequest();
 		String func = filledForm.get().function;
-		String ans = Calculator1.process(Calculator1.parse(func));
-		return ok(views.html.record.render(calcForm, func, ans));
+		if (func == null || func.equals("")) {
+			return ok(views.html.err.render(calcForm, "入力がありません", func));
+		} else {
+			String ans = Calculator1.process(Calculator1.parse(func));
+			if (ans == "エラー（０で割れません）" || ans == "エラー（逆ポーランド記法の数式を入力してください。）"
+					|| ans == "エラー（数字か演算子（+,-,*,/）かexitを入力してください）") {
+				return ok(views.html.err.render(calcForm, ans, func));
+			} else {
+				return ok(views.html.record.render(calcForm, ans, func));
+			}
+		}
 	}
+
 }
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,20 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table calculate (
+  function                  varchar(255))
+;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists calculate;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
